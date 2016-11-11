da99a8ccca38f76c3850d6b55475aacb395eab6a
1070138445@qq.com <1070138445@qq.com>
2016-11-11 14:43:02 +0800

    Initial commit

---
+++ app/src/main/java/com/yidou/wandou/Constances.java
@@ -0,0 +1,16 @@
+package com.yidou.wandou;
+
+/**
+ * Created by Administrator on 2016/11/11.
+ */
+
+public class Constances
+{
+    public static final String KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxx";
+    public static final String NEWS_URL = "http://v.juhe.cn/toutiao/";
+    public static final String TOP = "top";
+    public static final String SHI = "shishang";
+    public static final String JUN = "junshi";
+    public static final String Pictures = "http://apis.baidu.com/tngou/cook/";
+    public static final String IMGS = "http://tnfs.tngou.net/image";
+}
